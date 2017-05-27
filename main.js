c09d831aef8b84b115c01154b1d045d98c32d52b
terier <terier.kuza@gmail.com>
2017-05-27 20:03:36 +0200

    Refactor DOM positioning

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 $(function() {
     var renderer = new VolumeRenderer();
-    $('.fill').append($(renderer.getCanvas()));
+    var $canvas = $(renderer.getCanvas());
+    $canvas.addClass('renderer');
+    $(document.body).append($canvas);
 
     $(window).resize(function() {
         var w = window.innerWidth;
         var h = window.innerHeight;
-        $('.fill').css({
-            width: w,
-            height: h,
-            position: 'fixed'
-        });
         renderer.resize(w, h);
     });
     $(window).resize();
