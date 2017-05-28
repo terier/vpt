06f23a00f32a3f8c39ef0d517244be7ff2838109
terier <terier.kuza@gmail.com>
2017-05-28 16:42:46 +0200

    Gulp workflow WIP

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
+app.use('/static', express.static('build/css'));
+app.use('/static', express.static('build/images'));
+app.use('/static', express.static('build/js'));
+
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function () {
+    console.log('Listening on port ' + app.get('port'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
-var gulp    = require('gulp');
-var sass    = require('gulp-sass');
-var run     = require('gulp-run');
-var concat  = require('gulp-concat');
-var shell   = require('gulp-shell');
-var del     = require('del');
+var gulp       = require('gulp');
+var sass       = require('gulp-sass');
+var run        = require('gulp-run');
+var concat     = require('gulp-concat');
+var shell      = require('gulp-shell');
+var imagemin   = require('gulp-imagemin');
+var htmlmin    = require('gulp-htmlmin');
+var cache      = require('gulp-cache');
+var uglify     = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var htmlToJs   = require('gulp-html-to-js');
+var del        = require('del');
+
+// --------------------------------------------------------
 
 gulp.task('sass', function() {
     return gulp.src('app/css/**/*.scss')
@@ -12,6 +20,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('glsl', shell.task([
+    'mkdir -p build/js',
     'bin/concader -x -n MIXINS app/glsl/*.glsl > build/js/mixins.js',
     'bin/concader -n SHADERS app/glsl/*.frag app/glsl/*.vert > build/js/shaders.js',
 ]));
@@ -19,19 +28,50 @@ gulp.task('glsl', shell.task([
 gulp.task('js', function() {
     return gulp.src('app/js/**/*.js')
         .pipe(concat('main.js'))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('build/js'));
 });
+
+gulp.task('images', function(){
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg|ico|bmp)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('html', function() {
+    return gulp.src(['app/**/*.html', '!app/templates/**/*'])
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('templates', function() {
+    return gulp.src('app/templates/**/*.html')
+        .pipe(htmlToJs({
+            concat: 'templates.js',
+            global: 'TEMPLATES'
+        }))
+        .pipe(gulp.dest('build/js'));
+});
+
+// --------------------------------------------------------
 
 gulp.task('watch', function() {
     gulp.watch('app/css/**/*.scss', ['sass']);
     gulp.watch('app/js/**/*.js', ['js']);
-    gulp.watch('app/glsl/**', ['glsl']);
+    gulp.watch('app/images/**/*.+(png|jpg|jpeg|gif|svg|ico|bmp)', ['images']);
+    gulp.watch('app/glsl/**/*.(frag|vert|glsl)', ['glsl']);
+    gulp.watch(['app/**/*.html', '!app/templates/**/*'], ['html']);
+    gulp.watch(['app/templates/**/*'], ['templates']);
 });
 
 gulp.task('build', function() {
-    gulp.start('sass', 'js', 'glsl');
+    gulp.start('sass', 'js', 'glsl', 'images', 'html', 'templates');
 });
 
 gulp.task('clean', function() {
     return del('build');
 });
+
+gulp.task('default', ['watch']);
