2f7aa040f2aad354364b5888fb53d12fafe8e21d
terier <terier.kuza@gmail.com>
2017-06-03 13:02:10 +0200

    Add downloadJSON utility

--- app/js/utils.js
+++ app/js/utils.js
@@ -1,1 +1,10 @@
 function noop() {}
+
+function downloadJSON(json, filename) {
+    var str = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(json));
+    var a = document.createElement('a');
+    a.setAttribute('href', str);
+    a.setAttribute('download', filename);
+    a.click();
+    a = null;
+}
