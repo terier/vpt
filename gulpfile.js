3c01b6d4d9677e80a72a0ee117a5a57959ebbd94
terier <terier.kuza@gmail.com>
2017-06-05 17:26:42 +0200

    Remove gulp-run

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp       = require('gulp');
 var sass       = require('gulp-sass');
-var run        = require('gulp-run');
 var concat     = require('gulp-concat');
 var shell      = require('gulp-shell');
 var imagemin   = require('gulp-imagemin');
