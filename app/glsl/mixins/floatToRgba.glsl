e8a67299da71e048c1f791d30c869d64993dca34
terier <terier.kuza@gmail.com>
2017-06-22 19:43:45 +0200

    Add and reorganize glsl mixins

---
+++ app/glsl/mixins/floatToRgba.glsl
@@ -0,0 +1,6 @@
+vec4 floatToRgba(float x) {
+    const vec4 encoder = vec4(1.0, 255.0, 255.0 * 255.0, 255.0 * 255.0 * 255.0);
+    const vec4 corrector = vec4(1.0 / 255.0, 1.0 / 255.0, 1.0 / 255.0, 0.0);
+    vec4 enc = fract(x * encoder);
+    return enc - enc.yzww * corrector;
+}
---
+++ app/glsl/mixins/rgbaToFloat.glsl
@@ -0,0 +1,4 @@
+float rgbaToFloat(vec4 x) {
+    const vec4 decoder = 1.0 / vec4(1.0, 255.0, 255.0 * 255.0, 255.0 * 255.0 * 255.0);
+    return dot(x, decoder);
+}
