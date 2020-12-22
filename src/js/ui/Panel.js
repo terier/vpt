3063ed6f12779287927ba2a997372e251d6a7d15
Žiga Lesar <terier.kuza@gmail.com>
2020-12-22 19:20:07 +0100

    Temporary fix for sidebar scrolling

--- src/js/ui/Panel.js
+++ src/js/ui/Panel.js
@@ -6,6 +6,17 @@ class Panel extends UIObject {
 
 constructor(options) {
     super(TEMPLATES.ui.Panel, options);
+
+    Object.assign(this, {
+        scrollable: false
+    }, options);
+
+    this.setScrollable(this.scrollable);
+}
+
+setScrollable(scrollable) {
+    this.scrollable = scrollable;
+    this._element.classList.toggle('scrollable', scrollable);
 }
 
 add(object) {
