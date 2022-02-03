c7a7269df018cef1be163678338b3bafefbffd0c
hrosc <us6796@student.uni-lj.si>
2022-02-03 09:27:26 +0100

    Testing for IMC

--- src/js/renderers/IMCRenderer.js
+++ src/js/renderers/IMCRenderer.js
@@ -28,7 +28,10 @@ constructor(gl, volume, environmentTexture, options) {
         scatteringBias        : 0,
         majorant              : 100,
         maxBounces            : 8,
-        steps                 : 1
+        steps                 : 1,
+
+        // Testing
+        testing               : false
     }, options);
 
     this._programs = WebGL.buildPrograms(this._gl, {
@@ -39,6 +42,36 @@ constructor(gl, volume, environmentTexture, options) {
         resetISO        : SHADERS.IMCResetISO,
         resetMCM        : SHADERS.IMCResetMCM
     }, MIXINS);
+}
+
+startTesting(testingTime= 100, intervals= 100, saveAs = "MCC") {
+    // [0.4595949351787567, 0.017016194760799408, -0.002319802064448595, 1.8643269150686592e-9, 0.0008531888015568256, 0.009490122087299824, 0.37735629081726074, 2.2245090214312313e-9, -1.7914464473724365, 23.797161102294922, -2.561542272567749, -4.89999532699585, 1.85416579246521, -21.197429656982422, 2.650918960571289, 5.0999956130981445]
+    this._elapsedTime = 0
+    this.testingTime = testingTime * 1000;
+    this.testingIntervalTime = this.testingTime/intervals;
+    this.testingTotalTime = 0;
+    this.testing = true;
+    this.saveAs = saveAs;
+    this.reset();
+}
+
+testingProtocalSave() {
+    if (this.testing && this.testingTotalTime <= this.testingTime && this._elapsedTime >= this.testingIntervalTime) {
+        // console.log("saving")
+        let lastTime = this._elapsedTime;
+        this.testingTotalTime += lastTime;
+        // this._elapsedTime = lastTime - this.testingIntervalTime;
+        this._elapsedTime = 0
+        let canvas = document.getElementsByClassName("renderer")[0];
+        let link = document.createElement('a');
+        link.download = this.saveAs + "_" + this.testingTotalTime + ".png";
+        link.href = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        link.click();
+    }
+    else if (this.testingTotalTime > this.testingTime) {
+        this.testing = false;
+        console.log("Testing finished!")
+    }
 }
 
 destroy() {
@@ -58,6 +91,10 @@ reset() {
     gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
 
     this._resetFrame();
+
+    this._previousTime = new Date().getTime()
+    this._elapsedTime = 0;
+
 }
 
 _resetFrame() {
@@ -252,6 +289,13 @@ render() {
 
     this._renderBuffer.use();
     this._renderFrame();
+
+    if (this.testing) {
+        const currentTime = new Date().getTime();
+        this._elapsedTime += currentTime - this._previousTime;
+        this._previousTime = currentTime;
+        this.testingProtocalSave()
+    }
 }
 
 destroy() {
