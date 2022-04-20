8f5810d1cbbc4d3c3784991ce67001add98ceccc
Žiga Lesar <terier.kuza@gmail.com>
2022-04-20 17:07:00 +0200

    Fix tabs

--- src/js/ui/Tabs/Tabs.js
+++ src/js/ui/Tabs/Tabs.js
@@ -15,85 +15,41 @@ constructor() {
     this.shadow.appendChild(template.content.cloneNode(true));
     this.binds = DOMUtils.bind(this.shadow);
 
-    this.tabs = [];
-    this.index = 0;
+    // TODO: this should be done on every mutation
+    for (const header of this.binds.headers.assignedElements()) {
+        header.addEventListener('click', this.clickListener);
+    }
+
+    this.selectTab(0);
 }
 
-add(name, object) {
-    const panel = document.createElement('div');
-    const header = document.createElement('div');
-    const index = this.tabs.length;
+selectTab(index) {
+    const tabs = this.binds.tabs.assignedElements();
 
-    header.textContent = name || ('Tab ' + (index + 1));
-    this.tabs.push({
-        object : object,
-        header : header,
-        panel  : panel
-    });
-    this.binds.container.appendChild(panel);
-    this.binds.headers.appendChild(header);
-    object.appendTo(panel);
+    if (index < 0 || index >= tabs.length) {
+        throw new Error('Tab index out of range');
+    }
 
-    panel.style.order = index;
-    header.style.order = index;
-
-    header.classList.add('header');
-    header.addEventListener('click', this.clickListener);
-
+    this.index = index;
     this._updateStyle();
 }
 
-indexOfTab(tab) {
-    for (let i = 0; i < this.tabs.length; i++) {
-        if (this.tabs[i].header === tab ||
-            this.tabs[i].panel === tab ||
-            this.tabs[i].object === tab)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
-selectTab(objectOrIndex) {
-    const len = this.tabs.length;
-    if (len === 0) {
-        return;
-    }
-
-    let index;
-    if (typeof objectOrIndex === 'number') {
-        index = ((objectOrIndex % len) + len) % len;
-    } else {
-        index = this.indexOfTab(objectOrIndex);
-    }
-
-    if (index >= 0 && index <= len) {
-        this.index = index;
-        this._updateStyle();
-    }
-}
-
 _updateStyle() {
-    for (let i = 0; i < this.tabs.length; i++) {
-        const tab = this.tabs[i];
+    const tabs = this.binds.tabs.assignedElements();
+    const headers = this.binds.headers.assignedElements();
+
+    for (let i = 0; i < tabs.length; i++) {
         const offset = -this.index * 100;
-        tab.panel.style.left = offset + '%';
-        if (i === this.index) {
-            tab.header.classList.add('selected');
-            tab.panel.classList.add('selected');
-        } else {
-            tab.header.classList.remove('selected');
-            tab.panel.classList.remove('selected');
-        }
+        tabs[i].style.left = offset + '%';
+        tabs[i].classList.toggle('selected', i === this.index);
+        headers[i].classList.toggle('selected', i === this.index);
     }
 }
 
 clickListener(e) {
-    const index = this.indexOfTab(e.target);
-    if (index >= 0) {
-        this.selectTab(index);
-    }
+    const headers = this.binds.headers.assignedElements();
+    const index = headers.indexOf(e.target);
+    this.selectTab(index);
 }
 
 }
