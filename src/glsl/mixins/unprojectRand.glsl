7aa91058cc56735d4075fb7dcc672e200eb33121
Žiga Lesar <terier.kuza@gmail.com>
2022-05-31 21:06:00 +0200

    Use new random library in MCM

--- src/glsl/mixins/unprojectRand.glsl
+++ src/glsl/mixins/unprojectRand.glsl
@@ -1,7 +1,7 @@
 // #part /glsl/mixins/unprojectRand
 
 void unprojectRand(
-        inout vec2 randState,
+        inout uint state,
         in vec2 position,
         in mat4 inverseMvp,
         in vec2 inverseResolution,
@@ -9,16 +9,11 @@ void unprojectRand(
         out vec3 from, out vec3 to)
 {
     // sample a disk on the near plane (depth of field)
-    const float TWOPI = 2.0 * 3.14159265358979323;
-    randState = rand(randState);
-    float u1 = TWOPI * randState.x;
-    float u2 = sqrt(randState.y);
-    vec2 offset = vec2(cos(u1), sin(u1)) * u2 * blur;
+    vec2 offset = random_disk(state) * blur;
     vec4 nearPosition = vec4(position + offset, -1.0, 1.0);
 
     // sample a square on the far plane (antialiasing)
-    randState = rand(randState);
-    vec2 antialiasing = (randState * 2.0 - 1.0) * inverseResolution;
+    vec2 antialiasing = (random_square(state) * 2.0 - 1.0) * inverseResolution;
     vec4 farPosition = vec4(position + antialiasing, 1.0, 1.0);
 
     // map to world space
