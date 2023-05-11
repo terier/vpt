4734473c5d5cc63b4bccf22be2026f229428ce36
Žiga Lesar <terier.kuza@gmail.com>
2023-05-11 11:34:15 +0200

    Add references to sampling methods

--- src/glsl/mixins/random/distribution/normal.glsl
+++ src/glsl/mixins/random/distribution/normal.glsl
@@ -1,5 +1,6 @@
 // #part /glsl/mixins/random/distribution/normal
 
+// Box-Muller (1958)
 float random_normal(inout uint state) {
     float radius = sqrt(-2.0 * log(random_uniform(state)));
     float angle = TWOPI * random_uniform(state);
